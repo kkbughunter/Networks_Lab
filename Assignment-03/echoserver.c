4f31848ded8d06143ee8724b98f4527204e78fb2
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-10-14 13:57:31 +0530

    Update echoserver.c

--- Assignment-03/echoserver.c
+++ Assignment-03/echoserver.c
@@ -1,80 +1,52 @@
-#include <stdio.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h> // read(), write(), close()
-#define MAX 80
-#define PORT 8080
-#define SA struct sockaddr
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include <arpa/inet.h>
 
-void func(int connfd)
-{
-	char buff[MAX];
-	int n;
-	for (;;) {
-		bzero(buff, MAX);
+void messageToClient(int fd);
 
-		read(connfd, buff, sizeof(buff));
+void messageToClient(int fd){
+
+	char buf[1023];
+	
+	while(1){
+		read(fd, buf, sizeof(buf));
+		printf("Message Frome Client: %s\n", buf);
 		
-		printf("From client: %s", buff);
-		//bzero(buff, MAX);
-		//n = 0;
-		//while ((buff[n++] = getchar()) != '\n');
-
-		write(connfd, buff, sizeof(buff));
-
-		if (strncmp("exit", buff, 4) == 0) {
+		if ((strncmp(buf, "exit", 4)) == 0) {
 			printf("Server Exit...\n");
 			break;
 		}
+		
+		write(fd, buf, sizeof(buf));
 	}
+	
+	return ;
+	
 }
 
-int main()
-{
-	int sockfd, connfd, len;
-	struct sockaddr_in servaddr, cli;
-
+int main(int argc, char *args[]){
+	
+	int sockfd;
+	struct sockaddr_in server;
+	int len = sizeof(server);
+	
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		printf("socket creation failed...\n");
-		exit(0);
-	}
-	else
-		printf("Socket successfully created..\n");
-	bzero(&servaddr, sizeof(servaddr));
-
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(PORT);
-
-	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-		printf("socket bind failed...\n");
-		exit(0);
-	}
-	else
-		printf("Socket successfully binded..\n");
-
-	if ((listen(sockfd, 5)) != 0) {
-		printf("Listen failed...\n");
-		exit(0);
-	}
-	else
-		printf("Server listening..\n");
-	len = sizeof(cli);
-
-	connfd = accept(sockfd, (SA*)&cli, &len);
-	if (connfd < 0) {
-		printf("server accept failed...\n");
-		exit(0);
-	}
-	else
-		printf("server accept the client...\n");
-
-	func(connfd);
+	
+	server.sin_family = AF_INET;
+	server.sin_port = htons(7020);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	int binderr = bind(sockfd, (struct sockaddr *)&server, len);
+	int listenerr = listen(sockfd, 4);
+	printf("Server Listening to port 7020....\n\n");
+	int fd = accept(sockfd, (struct sockaddr *)&server, &len);
+	printf("%s Client is connect to Server...\n",inet_ntoa(server.sin_addr));
+	messageToClient(fd);
 
 	close(sockfd);
 }
