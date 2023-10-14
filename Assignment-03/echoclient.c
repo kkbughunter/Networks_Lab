c1185a93830d38e48a1e99fcd3e90f3ab2a0ac70
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-10-14 13:57:15 +0530

    Update echoclient.c

--- Assignment-03/echoclient.c
+++ Assignment-03/echoclient.c
@@ -1,63 +1,51 @@
-#include <arpa/inet.h> // inet_addr()
-#include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h> // bzero()
-#include <sys/socket.h>
-#include <unistd.h> // read(), write(), close()
-#define MAX 80
-#define PORT 8080
-#define SA struct sockaddr
-void func(int sockfd)
-{
-	char buff[MAX];
-	int n;
-	for (;;) {
-		bzero(buff, sizeof(buff));
-		printf("Enter the string : ");
-		n = 0;
-		while ((buff[n++] = getchar()) != '\n');
-		write(sockfd, buff, sizeof(buff));
-		bzero(buff, sizeof(buff));
-		read(sockfd, buff, sizeof(buff));
-		printf("From Server : %s", buff);
-		if ((strncmp(buff, "exit", 4)) == 0) {
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+void messageToServer(int sockfd);
+
+void messageToServer(int sockfd){
+	
+	char buf[1023];
+	
+	for(;;){
+		printf("Enter the message to send: ");
+		fgets(buf, 1023, stdin);
+		buf[sizeof(buf)-1] = '\0';
+		
+		write(sockfd, buf, sizeof(buf)); 
+		
+		if ((strncmp(buf, "exit", 4)) == 0) {
 			printf("Client Exit...\n");
 			break;
 		}
+		
+		read(sockfd, buf, sizeof(buf));
+		printf("Message Frome Server: %s\n", buf);
 	}
 }
 
-int main()
-{
-	int sockfd, connfd;
-	struct sockaddr_in servaddr, cli;
+int main(int argc, char *args[]){
 
-
+	int sockfd;
+	struct sockaddr_in client;
+	int len = sizeof(client);
+	
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		printf("socket creation failed...\n");
-		exit(0);
-	}
-	else
-		printf("Socket successfully created..\n");
-	bzero(&servaddr, sizeof(servaddr));
-
-
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	servaddr.sin_port = htons(PORT);
-
-	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
-		!= 0) {
-		printf("connection with the server failed...\n");
-		exit(0);
-	}
-	else
-		printf("connected to the server..\n");
-
-	func(sockfd);
-
-	close(sockfd);
+	
+	client.sin_family = AF_INET;
+	client.sin_port = htons(7020);
+	client.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	int binderr = bind(sockfd, (struct sockaddr *)&client, len);
+	
+	int connecterr = connect(sockfd, (struct sockaddr *)&client, len);
+	
+	messageToServer(sockfd);
+	
+	return 0;
 }
